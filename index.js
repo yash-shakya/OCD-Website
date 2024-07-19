a959d79161e7216d9cd0e3460d094db9b778c684
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2024-07-19 12:58:02 +0530

    Created A section for pop-up

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ contactForm.addEventListener('submit', function(event) {
         formResponse.style.display = 'none';
     }, 2000);
 });
+
+// JS code for Pop-up section 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var popupBanner = document.getElementById('popup-banner');
+    var closeBtn = document.getElementById('close-btn');
+
+    // Function to show the popup banner
+    function showPopup() {
+        popupBanner.style.display = 'block';
+    }
+
+    // Function to close the popup banner
+    closeBtn.addEventListener('click', function() {
+        popupBanner.style.display = 'none';
+    });
+
+    // Show the popup after a delay (e.g., 3 seconds)
+    setTimeout(showPopup, 1000);
+});
