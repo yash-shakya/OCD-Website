bf8349e0f419230a2e8de95726630d2df939dd5e
Kharb-Hardik <hardik.kharb05@gmail.com>
2024-07-20 22:55:47 +0530

    fixed Dark mode functionality with icons

--- js/index.js
+++ js/index.js
@@ -12,18 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const currentMode = localStorage.getItem('theme') || 'light';
     document.body.className = `${currentMode}-mode`;
-    toggleButton.textContent = currentMode === 'light' ? 'Dark Mode' : 'Light Mode';
+    // toggleButton.textContent = currentMode === 'light' ? 'Dark Mode' : 'Light Mode'; // redundant as we now have icons 
 
     toggleButton.addEventListener('click', () => {
         const isDarkMode = document.body.classList.contains('dark-mode');
         
         if (isDarkMode) {
             document.body.className = 'light-mode';
-            toggleButton.textContent = 'Dark Mode';
+            // toggleButton.textContent = 'Dark Mode';// redundant as we now have icons 
             localStorage.setItem('theme', 'light');
         } else {
             document.body.className = 'dark-mode';
-            toggleButton.textContent = 'Light Mode';
+            // toggleButton.textContent = 'Light Mode';// redundant as we now have icons 
             localStorage.setItem('theme', 'dark');
         }
     });
